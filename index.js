fbf0854055026b5649da13622274fb7195b0225d
justinvdm <justinvdm.0.1.0@gmail.com>
2016-10-23 12:56:39 +0200

    refactor to avoid many function creations each dispatch

--- index.js
+++ index.js
@@ -9,7 +9,25 @@ function input() {
 
 
 function create(defs) {
-  return new Graph(buildGraph(defs));
+  var inputs = buildGraph(defs);
+  var self = {dispatch: dispatch};
+  return self;
+
+  function dispatch(def, msgs, done) {
+    done = done || noop;
+    done = callOnNth(inputs.length, done);
+
+    var n = inputs.length;
+    var i = -1;
+    var node;
+
+    while (++i < n) {
+      node = inputs[i];
+      if (node.def === def) node.handle(msgs, done);
+    }
+
+    return this;
+  }
 }
 
 
@@ -33,34 +51,11 @@ function trap(type, obj) {
 }
 
 
-Graph.prototype.dispatch = function dispatch(def, msgs, done) {
-  done = done || noop;
-  done = callOnNth(this.inputs.length, done);
-
-  var inputs = this.inputs;
-  var n = inputs.length;
-  var i = -1;
-  var node;
-
-  while (++i < n) {
-    node = inputs[i];
-    if (node.def === def) receive(node.child, msgs, node.parentIndex, done);
-  }
-
-  return this;
-};
-
-
 // types
 
 function ValueMsgType() {}
 function ErrorMsgType() {}
 function NilMsgType() {}
-
-
-function UnhandledError(error) {
-  this.error = error;
-}
 
 
 function Msg(type, value) {
@@ -79,43 +74,35 @@ function InputDef() {
 
 
 function ProcessorDef(opts) {
+  opts = castProcessorShape(opts);
   this.type = ensure(opts.type, ValueMsgType);
   this.init = ensure(opts.init, noop);
   this.process = opts.process;
 }
 
 
-function InputNode(def, child, parentIndex) {
+function Node(def, child, index) {
   this.def = def;
   this.child = child;
-  this.parentIndex = parentIndex;
+  this.index = index;
+  this.handle = createHandler(this);
 }
 
 
-function ProcessorNode(def, child, parentIndex) {
-  this.def = def;
-  this.child = child;
-  this.parentIndex = parentIndex;
-  this.state = this.def.init();
-  this.queue = [];
-  this.isBusy = false;
-}
-
-
-function Graph(inputs) {
-  this.inputs = inputs;
+function UnhandledError(error) {
+  this.error = error;
 }
 
 
 // graph building
 
-function buildGraph(defs, child, parentIndex) {
+function buildGraph(defs, child, index) {
   // TODO validate not empty
   var i = defs.length - 1;
 
   // tail
-  if (i) child = createProcessorNode(defs[i], child, parentIndex);
-  while (--i) child = createProcessorNode(defs[i], child, 0);
+  if (i) child = new Node(new ProcessorDef(defs[i]), child, index);
+  while (--i) child = new Node(new ProcessorDef(defs[i]), child, 0);
 
   return buildGraphHead(defs[0], child);
 }
@@ -136,7 +123,7 @@ function buildGraphHead(defs, child) {
       // TODO check for empty array
       inputs.push.apply(inputs, buildGraph(def, child, i));
     } else if (def instanceof InputDef) {
-      inputs.push(new InputNode(def, child, i));
+      inputs.push(new Node(def, child, i));
     } else {
       // TODO throw error
     }
@@ -146,72 +133,95 @@ function buildGraphHead(defs, child) {
 }
 
 
-function createProcessorNode(def, child, i) {
-  // TODO validate def
-  return new ProcessorNode(new ProcessorDef(castProcessorShape(def)), child, i);
+function createHandler(node) {
+  return {
+    InputDef: createInputHandler,
+    ProcessorDef: createProcessorHandler
+  }[node.def.constructor.name](node);
 }
 
 
-// message processing
-
-function schedule(node, task) {
-  node.queue.push(task);
-  if (!node.isBusy) processNext(node);
+function createInputHandler(node) {
+  return function handle(msgs, end) {
+    node.child.handle(msgs, node, end);
+  };
 }
 
 
-function processNext(node) {
-  var task = node.queue.shift();
-  if (task) process.apply(null, [node].concat(task));
-}
+function createProcessorHandler(node) {
+  var queue = [];
+  var isBusy = false;
+  var state = node.def.init();
+  var task = null;
 
+  var processFnAsync = maybeAsync(processFn);
+  return handle;
 
-function process(node, msg, i, done) {
-  node.isBusy = true;
+  function handle(msgs, parent, end) {
+    msgs = castBatch(msgs).messages;
 
-  maybeAsync(processFn)()
-    .then(resolveSync)
-    .then(success, failure)
-    .then(null, unhandledError);
+    var i = -1;
+    var n = msgs.length - 1;
+
+    while (++i < n) schedule({
+      msg: msgs[i],
+      parent: parent,
+      end: noop
+    });
+
+    if (n > -1) schedule({
+      msg: msgs[n],
+      parent: parent,
+      end: end
+    });
+  }
+
+  function schedule(task) {
+    queue.push(task);
+    if (!isBusy) processNext();
+  }
+
+  function processNext() {
+    task = queue.shift();
+    if (task) process();
+  }
+
+  function process() {
+    isBusy = true;
+
+    return processFnAsync()
+      .then(resolveSync)
+      .then(success, failure)
+      .then(done);
+  }
 
   function processFn() {
-    return node.def.type === msg.type
-      ? node.def.process(node.state, msg.value, i)
-      : [node.state, msg];
+    return node.def.type === task.msg.type
+      ? node.def.process(state, task.msg.value, task.parent.index)
+      : [state, task.msg];
+  }
+
+  function done(msgs) {
+    var end = task.end;
+    isBusy = false;
+    task = null;
+    processNext();
+    if (node.child) node.child.handle(msgs, node, end);
+    else end();
   }
 
   function success(res) {
     res = !Array.isArray(res)
-      ? [node.state, nil]
+      ? [state, nil]
       : res;
 
-    node.state = res[0];
-    node.isBusy = false;
-    done(null, res[1]);
-    processNext(node);
+    state = res[0];
+    return res[1];
   }
 
   function failure(e) {
-    node.isBusy = false;
-    done(e);
-    processNext(node);
-  }
-}
-
-
-function receive(node, msgs, parentIndex, end) {
-  msgs = castBatch(msgs).messages;
-  end = callOnNth(msgs.length, end);
-
-  var i = -1;
-  var n = msgs.length;
-  while (++i < n) schedule(node, [msgs[i], parentIndex, done]);
-
-  function done(err, res) {
-    if (err) res = message(ErrorMsgType, err);
-    if (node.child) receive(node.child, res, node.parentIndex, end);
-    else if (err) throw err;
-    else end();
+    if (!node.child) throw new UnhandledError(e);
+    return message(ErrorMsgType, e);
   }
 }
 
@@ -248,6 +258,16 @@ function ensure(v, defaultVal) {
 }
 
 
+function identity(v) {
+  return v;
+}
+
+
+function throwError(e) {
+  throw e;
+}
+
+
 function castBatch(v) {
   return !(v instanceof Batch)
     ? batch([v])
@@ -276,57 +296,61 @@ function castProcessorShape(v) {
 }
 
 
+function callOnNth(n, fn) {
+  var i = 0;
+
+  return function onNthFn() {
+    if (++i >= n) fn.apply(null, arguments);
+  };
+}
+
+
+
 function maybeAsync(fn) {
-  return function maybeAsync() {
+  return maybeAsyncFn;
+
+  function maybeAsyncFn() {
     try {
-      var v = fn.apply(null, arguments);
+      var v = fn.apply(this, arguments);
       return castThenable(v);
     } catch (e) {
       if (e instanceof UnhandledError) throw e.error;
-      else return thenableError(e);
+      return thenableError(e);
     }
   }
 }
 
 
 function castThenable(v) {
-  return !(v || 0).then
+  return !isThenable(v)
     ? thenableValue(v)
     : v;
 }
 
 
 function thenableValue(v) {
-  return {then: maybeAsync(successFn)};
-
-  function successFn(success) {
-    return success(v);
-  }
+  return new Thenable(v, false);
 }
 
 
 function thenableError(e) {
-  return {then: maybeAsync(failureFn)};
-
-  function failureFn(_, failure) {
-    if (failure) failure(e);
-    else throw e;
-  }
-}
-
-
-function unhandledError(e) {
-  throw new UnhandledError(e);
+  return new Thenable(e, true);
 }
 
 
 function resolveSync(values) {
-  if (!Array.isArray(values)) return castThenable(values);
+  return Array.isArray(values) && values.filter(isThenable).length > 0
+    ? resolveThenablesSync(values)
+    : castThenable(values);
+}
+
+
+function resolveThenablesSync(values) {
   var res = [];
   var n = values.length;
   var i = -1;
   var j = -1;
-  var p = castThenable(null);
+  var p = thenableValue(null);
 
   while (++i < n) p = p.then(call);
   return p.then(done);
@@ -345,13 +369,23 @@ function resolveSync(values) {
 }
 
 
-function callOnNth(n, fn) {
-  var i = 0;
-
-  return function onNthFn() {
-    if (++i >= n) fn.apply(null, arguments);
-  };
+function isThenable(v) {
+  return !!(v || 0).then;
 }
+
+
+function Thenable(value, isFailure) {
+  this.value = value;
+  this.isFailure = isFailure;
+}
+
+
+Thenable.prototype.then = maybeAsync(function then(successFn, failureFn) {
+  successFn = successFn || identity;
+  failureFn = failureFn || throwError;
+  if (this.isFailure) return failureFn(this.value);
+  else return successFn(this.value);
+});
 
 
 module.exports = {
