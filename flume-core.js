3c57e170ee1623f0f4a8ff252496c9a0df5e20d6
justinvdm <justinvdm.0.1.0@gmail.com>
2017-06-18 13:13:44 +0200

    start of next

--- flume-core.js
+++ flume-core.js
@@ -1,456 +1,236 @@
+// @flow
 ;(function(root, factory) {
   if (typeof root.define === 'function' && root.define.amd) root.define(factory);
   else if (typeof exports === 'object') factory(true);
+   // $FlowFixMe
   else root.flume = factory();
 })(this, function(cjs) {
-  // api
+  /*::
+  type DefType<Type, Parents, Description> = {|
+    type: Type,
+    parents: Parents,
+    description: Description
+  |};
 
-  var nil = message('flume:nil', null);
+  type InputDef = DefType<'input', null, null>;
+  type TransformDef = DefType<'transform', Def[], TransformDescription>;
 
+  type TransformDescription = {|
+    msgType: string,
+    init: TransformInitFn,
+    transform: TransformFn
+  |};
 
-  function input() {
-    return new InputDef();
-  }
+  type Def = InputDef | TransformDef;
 
+  type TransformFn = (state: any, value: any) => any;
 
-  function create(defs) {
-    var self = {};
-    self.dispatch = dispatch;
-    self.inputs = buildGraph(self, defs);
+  type TransformInitFn = () => any;
 
-    return self;
+  type Msg = {|
+    type: string,
+    value: any
+  |};
 
-    function dispatch(src, msgs, done) {
-      var nodes = inputsOf(self.inputs, src);
-      var n = nodes.length;
-      var i = -1;
+  type Task = {|
+    msg: Msg,
+    source: InputDef,
+    parent: Def
+  |}
 
-      done = callOnNth(n, done || noop);
-      while (++i < n) nodes[i].handle(msgs, done);
+  type Graph = {|
+    inputs: Node[]
+  |};
 
-      return self;
-    }
-  }
+  type NodeType<Def, Child, State> = {|
+    def: Def,
+    child: ?Child,
+    state: State,
+    parentIndex: number
+  |};
 
+  type InputNode = NodeType<InputDef, TransformNode, null>;
+  type TransformNode = NodeType<TransformDef, TransformNode, TransformState>;
+  type Node = InputNode | TransformNode;
 
-  function message(type, v) {
-    return new Msg(type, v);
-  }
+  type TransformState = {|
+    tasks: Task[],
+    status: 'idle' | 'busy',
+    data: any
+  |}
+  */
 
-
-  function except(fn) {
-    return trap({'flume:error': fn});
-  }
-
-
-  function trap(transform) {
+  function input()/*:InputDef*/ {
     return {
-      transform: conj({'*': trapFallbackMsgHandler}, transform)
+      type: 'input',
+      parents: null,
+      description: null
     };
   }
 
-
-  // types
-
-  function Msg(type, value) {
-    this.type = type;
-    this.value = value;
+  function transform(init/*:TransformInitFn*/, transform/*:TransformFn*/) {
+    return function transformFn(parents/*:Def | Def[]*/)/*:TransformDef*/ {
+      return {
+        parents: castArray(parents),
+        type: 'transform',
+        description: {
+          init: init,
+          msgType: '__value',
+          transform: transform
+        }
+      };
+    };
   }
 
+  function map(fn/*:Function*/)/*:TransformFn*/ {
+    return transform(retNull, mapFn);
 
-  function InputDef() {
-    this.defType = 'input';
+    function mapFn(_, v) {
+      return [null, fn(v)];
+    }
   }
 
-
-  function TransformDef(opts) {
-    this.defType = 'transform';
-    this.init = ensure(opts.init, noop);
-    this.transform = opts.transform;
-  }
-
-
-  function Node(graph, def, child, index) {
-    this.graph = graph;
-    this.def = def;
-    this.child = child;
-    this.index = index;
-    this.handle = createHandler(this);
-  }
-
-
-  // graph building
-
-  function buildGraph(graph, defs, child, index) {
-    var i = defs.length - 1;
-    if (i < 0) return [];
-
-    // tail
-    if (i) child = new Node(graph, createTransformDef(defs[i]), child, index);
-    while (--i > 0) child = new Node(graph, createTransformDef(defs[i]), child, 0);
-
-    return buildGraphHead(graph, defs[0], child);
-  }
-
-
-  function buildGraphHead(graph, defs, child) {
-    defs = castArray(defs);
-
+  function create(tailDef/*:TransformDef*/)/*:Graph*/ {
     var inputs = [];
-    var i = -1;
-    var def;
-    var n = defs.length;
+    var queue = [createTransformNode(tailDef, null, 0)];
+    var i;
+    var n;
+    var node;
+    var parentDefs;
+    var parentDef;
 
-    while (++i < n) {
-      def = defs[i];
+    while (node = queue.shift()) {
+      parentDefs = node.def.parents;
+      i = -1;
+      n = parentDefs.length;
 
-      if (Array.isArray(def)) {
-        inputs.push.apply(inputs, buildGraph(graph, def, child, i));
-      } else if (def instanceof InputDef) {
-        inputs.push(new Node(graph, def, child, i));
-      } else {
-        throw new Error("Expected input or array but got " + typeOf(def));
+      while (++i < n) {
+        parentDef = parentDefs[i];
+
+        if (parentDef.type === 'input') {
+          inputs.push(createInputNode(parentDef, node, i));
+        } else {
+          queue.push(createTransformNode(parentDef, node, i));
+        }
       }
     }
 
-    return inputs;
+    return {inputs: inputs};
   }
 
-
-  function createTransformDef(obj) {
-    if (typeof obj === 'function') {
-      obj = {
-        transform: {'flume:value': obj}
-      };
-    }
-    else if (typeof (obj || 0).transform === 'function') {
-      obj = conj(obj, {
-        transform: {'flume:value': obj.transform}
-      });
-    }
-    else if (typeOf((obj || 0).transform) !== 'object') {
-      throw new Error(
-        "Expected function or object matching transform shape but got " +
-        typeOf(obj));
-    }
-
-    return new TransformDef(obj);
-  }
-
-
-  // message transforming
-
-  function trapFallbackMsgHandler(state, v) {
-    return {state: v};
-  }
-
-
-  function createHandler(node) {
-    return {
-      input: createInputHandler,
-      transform: createTransformHandler
-    }[node.def.defType](node);
-  }
-
-
-  function createInputHandler(node) {
-    return function handle(msg, end) {
-      node.child.handle([msg], node, node, end);
-    };
-  }
-
-
-  function createTransformHandler(node) {
-    var queue = [];
-    var isBusy = false;
-    var task = null;
-    var state = node.def.init();
-    var transformAsync = maybeAsync(transform);
-
-    return function handle(msgs, parent, source, end) {
-      msgs = msgs.map(castMessage)
-      var i = -1;
-      var n = msgs.length - 1;
-
-      var taskDefaults = {
-        source: source,
-        parent: parent,
-      };
-
-      while (++i < n) schedule(conj(taskDefaults, {
-        msg: msgs[i],
-        end: noop
-      }));
-
-      if (n > -1) schedule(conj(taskDefaults, {
-        msg: msgs[n],
-        end: end
-      }));
-    };
-
-    function schedule(task) {
-      queue.push(task);
-      if (!isBusy) next();
-    }
-
-    function next() {
-      task = queue.shift();
-      if (task) run();
-    }
-
-    function run() {
-      isBusy = true;
-
-      return transformAsync()
-        .then(resolveSeq)
-        .then(success, failure);
-    }
-
-    function transform() {
-      var fns = node.def.transform;
-      var fn = fns[task.msg.type] || fns['*'];
-      return !fn
-        ? {value: task.msg}
-        : fn(state, task.msg.value, {
-          source: task.source.def,
-          parent: task.parent.def,
-          dispatch: node.graph.dispatch
-        });
-    }
-
-    function done(msgs) {
-      var currTask = task;
-      isBusy = false;
-      task = null;
-
-      if (node.child) node.child.handle(msgs, node, currTask.source, currTask.end);
-      else currTask.end();
-
-      next();
-    }
-
-    function success(res) {
-      res = normalizeResult(state, res);
-      state = res.state;
-      done(res.values);
-    }
-
-    function failure(e) {
-      if (!node.child) throw new UnhandledError(e);
-      done([message('flume:error', e)]);
-    }
-  }
-
-
-  // utils
-
-  function inputsOf(inputs, def) {
-    var res = [];
+  function dispatch(graph/*:Graph*/, source/*:InputDef*/, value/*:any*/)/*:Graph*/ {
+    var inputs = graph.inputs;
     var n = inputs.length;
     var i = -1;
     var input;
 
     while (++i < n) {
       input = inputs[i];
-      if (input.def === def) res.push(input);
+
+      if (input.def === source && input.child) {
+        processTask(input.child, createTask(source, input.def, value));
+      }
     }
 
-    return res;
+    return graph;
   }
 
-
-  function conj() {
-    var res = {};
-    var objects = Array.prototype.slice.call(arguments);
-
-    var i = -1;
-    var n = objects.length;
-    var obj;
-
-    while (++i < n) {
-      obj = objects[i];
-      for (var k in obj) if (obj.hasOwnProperty(k)) res[k] = obj[k]
-    }
-
-    return res;
-  }
-
-
-  function noop() {
-    return null;
-  }
-
-
-  function ensure(v, defaultVal) {
-    return typeof v === 'undefined'
-      ? defaultVal
-      : v;
-  }
-
-
-  function identity(v) {
-    return v;
-  }
-
-
-  function throwError(e) {
-    throw e;
-  }
-
-
-  function castMessage(v) {
-    return !(v instanceof Msg)
-      ? message('flume:value', v)
-      : v;
-  }
-
-
-  function normalizeResult(state, raw) {
-    var hasState = raw.hasOwnProperty('state');
-    var values;
-
-    if (raw.hasOwnProperty('value')) values = [raw.value];
-    else if (raw.hasOwnProperty('values')) values = raw.values;
-    else if (hasState) values = [raw.state];
-
+  function createInputNode(def/*:InputDef*/, child/*:?TransformNode*/, parentIndex/*:number*/)/*:InputNode*/ {
     return {
-      values: values,
-      state: hasState
-        ? raw.state
-        : state
+      def: def,
+      child: child,
+      parentIndex: parentIndex,
+      state: null
     };
   }
 
+  function createTransformNode(def/*:TransformDef*/, child/*:?TransformNode*/, parentIndex/*:number*/)/*:TransformNode*/ {
+    return {
+      def: def,
+      child: child,
+      parentIndex: parentIndex,
+      state: {
+        tasks: [],
+        status: 'idle',
+        data: def.description.init()
+      }
+    };
+  }
 
-  function castArray(v) {
+  function createValueMsg(value/*:any*/)/*:Msg*/ {
+    return {
+      type: '__value',
+      value: value
+    };
+  }
+
+  function createTask(source/*:InputDef*/, parent/*:Def*/, value/*:any*/) {
+    return {
+      source: source,
+      parent: parent,
+      msg: createValueMsg(value)
+    };
+  }
+
+  function castArray(v/*:any*/)/*:any[]*/ {
     return !Array.isArray(v)
       ? [v]
       : v;
   }
 
-
-  function typeOf(v) {
-    return v !== null
-      ? typeof v
-      : 'null';
+  function retNull()/*:null*/ {
+    return null;
   }
 
-
-  function callOnNth(n, fn) {
-    var i = 0;
-
-    return function onNthFn() {
-      if (++i >= n) fn.apply(null, arguments);
-    };
+  function processTask(node/*:TransformNode*/, task/*:Task*/) {
+    var state = node.state;
+    if (state.status === 'idle') runTask(node, task);
+    else state.tasks.push(task);
   }
 
-
-  function maybeAsync(fn) {
-    return function maybeAsyncFn() {
-      try {
-        var v = fn.apply(this, arguments);
-        return castThenable(v);
-      } catch (e) {
-        if (e instanceof UnhandledError) throw e.error;
-        return thenableError(e);
-      }
-    }
+  function runNextTask(node/*:TransformNode*/) {
+    var task = node.state.tasks.shift();
+    if (task) runTask(node, task);
   }
 
+  function runTask(node/*:TransformNode*/, task/*:Task*/) {
+    var state = node.state;
+    var description = node.def.description;
+    state.status = 'busy';
 
-  function castThenable(v) {
-    return !isThenable(v)
-      ? thenableValue(v)
-      : v;
+    var res = description.transform(state.data, task.msg.value);
+
+    state.data = res[0];
+    state.status = 'idle';
+
+    if (node.child) processTask(node.child, createTask(task.source, node.def, res[1]));
+    runNextTask(node);
   }
 
-
-  function thenableValue(v) {
-    return new Thenable(v, false);
-  }
-
-
-  function thenableError(e) {
-    return new Thenable(e, true);
-  }
-
-
-  function resolveSeq(values) {
-    return Array.isArray(values) && values.filter(isThenable).length > 0
-      ? resolveThenablesSeq(values)
-      : castThenable(values);
-  }
-
-
-  function resolveThenablesSeq(values) {
-    var res = [];
-    var n = values.length;
+  function pipe(v/*:any*/, fns/*:Function[]*/)/*:any*/ {
     var i = -1;
-    var j = -1;
-    var p = thenableValue(null);
+    var n = fns.length;
 
-    while (++i < n) p = p.then(call);
-    return p.then(done);
+    while (++i < n) v = fns[i](v);
 
-    function call() {
-      return castThenable(values[++j]).then(push);
-    }
-
-    function push(v) {
-      res.push(v);
-    }
-
-    function done() {
-      return res;
-    }
+    return v;
   }
 
-
-  function isThenable(v) {
-    return !!(v || 0).then;
-  }
-
-
-  function UnhandledError(error) {
-    this.error = error;
-  }
-
-
-  function Thenable(value, isFailure) {
-    this.value = value;
-    this.isFailure = isFailure;
-  }
-
-
-  Thenable.prototype.then = maybeAsync(function then(successFn, failureFn) {
-    if (this.isFailure) return (failureFn || throwError).call(this, this.value);
-    else return (successFn || identity).call(this, this.value);
-  });
-
-
-  if (cjs)
-    module.exports = {
-      create: create,
-      input: input,
-      message: message,
-      trap: trap,
-      except: except,
-      nil: nil,
-      conj: conj,
-      maybeAsync: maybeAsync,
-      resolveSeq: resolveSeq
-    }
-  else
+  if (cjs) {
+    exports.pipe = pipe;
+    exports.input = input;
+    exports.create = create;
+    exports.dispatch = dispatch;
+    exports.transform = transform;
+    exports.map = map;
+  } else {
     return {
-      create: create,
       input: input,
-      message: message,
-      trap: trap,
-      except: except,
-      nil: nil,
-      conj: conj,
-      maybeAsync: maybeAsync,
-      castThenable: castThenable,
-      resolveSeq: resolveSeq
+      pipe: pipe,
+      create: create,
+      dispatch: dispatch,
+      transform: transform,
+      map: map
     };
+  }
 });
