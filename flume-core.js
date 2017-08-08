38c4856566bbd2d08aaa249cecc910dc73512eb3
justinvdm <justinvdm.0.1.0@gmail.com>
2017-08-08 22:22:51 +0200

    move joinEvents to flume-js

--- flume-core.js
+++ flume-core.js
@@ -155,32 +155,6 @@
     return map(branch(fn, identity, retNil));
   }
 
-  function joinEvents(mapFn/*:Function*/, inputs/*:MapType<InputDef>*/) {
-    return transform(initFn, transformFn);
-
-    function transformFn(events/*:MapType<Function>*/, v/*:any*/)/*:[MapType<Function>, any]*/ {
-      return [events, mapFn(events, v)];
-    }
-
-    function initFn(graph/*:Graph*/)/*:MapType<Function>*/ {
-      var events = {};
-
-      for (var k in inputs) {
-        if (inputs.hasOwnProperty(k)) {
-          events[k] = createEventDispatcher(graph, inputs[k]);
-        }
-      }
-
-      return events;
-    }
-  }
-
-  function createEventDispatcher(graph/*:Graph*/, input/*:InputDef*/)/*:Function*/ {
-    return function eventDispatcherFn(v/*:any*/) {
-      dispatch(graph, input, v);
-    };
-  }
-
   function trap(msgType/*:string*/, fn/*:TransformDefFn*/)/*:TransformDefFn*/ {
     return function exceptFn(parents/*:**/)/*:**/ {
       var def = fn(parents);
@@ -629,6 +603,5 @@
   exports.branch = branch;
   exports.msg = msg;
   exports.list = list;
-  exports.joinEvents = joinEvents;
   exports.identity = identity;
 });
